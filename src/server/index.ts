df48e1a8a0a4d348413a481b7daa35bd2b617a84
Revoltzin <jat1963@gmail.com>
2026-03-16 20:32:00 -0300

    SignUp feature implemented

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,28 @@
 import express from 'express'
+import { SignUpController } from '../application/controllers/SignUpController.js'
+import { SignUpUseCase } from '../application/useCases/SignUpUseCase.js'
 
 const app = express()
+app.use(express.json())
+
+app.post('/sign-up', async (request, response) => {
+    const SALT = 10
+    const signUpUseCase = new SignUpUseCase(SALT)
+    const signUpController = new SignUpController(signUpUseCase)
+
+    const { statusCode, body } = await signUpController.handle({
+        body: request.body
+    })
+
+    response.status(statusCode).json(body)
+})
+
+app.post('/sign-in', (request, response) => {
+    response.status(200).json({
+        message: 'SignIn'
+    })
+})
 
 app.listen(3001, () => {
-    console.log('Server Runnign')
+    console.log('Server Running')
 })
