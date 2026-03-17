c8e05891f04a5d6d4d39e5c22165ff188b629e2f
Revoltzin <jat1963@gmail.com>
2026-03-17 17:21:31 -0300

    routeadapter implemented

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,35 +1,17 @@
 import express from 'express'
-import { SignUpController } from '../application/controllers/SignUpController.js'
-import { SignUpUseCase } from '../application/useCases/SignUpUseCase.js'
-import { SignInController } from '../application/controllers/SignInController.js'
-import { SignInUseCase } from '../application/useCases/SignInUseCase.js'
+// import { SignUpController } from '../application/controllers/SignUpController.js'
+// import { SignUpUseCase } from '../application/useCases/SignUpUseCase.js'
+// import { SignInController } from '../application/controllers/SignInController.js'
+// import { SignInUseCase } from '../application/useCases/SignInUseCase.js'
+import { routeAdapter } from './adapters/routeAdapter.js'
+import { makeSignUpController } from '../factories/makeSignUpController.js'
 
 const app = express()
 app.use(express.json())
 
-app.post('/sign-up', async (request, response) => {
-    const SALT = 10
-    const signUpUseCase = new SignUpUseCase(SALT)
-    const signUpController = new SignUpController(signUpUseCase)
+app.post("/sign-up", routeAdapter(makeSignUpController()))
 
-    const { statusCode, body } = await signUpController.handle({
-        body: request.body
-    })
-
-    response.status(statusCode).json(body)
-})
-
-app.post('/sign-in', async (request, response) => {
-
-    const signInUseCase = new SignInUseCase()
-    const signInController = new SignInController(signInUseCase)
-
-    const { statusCode, body } = await signInController.handle({
-        body: request.body
-    })
-
-    response.status(statusCode).json(body)
-})
+app.post("/sign-in", routeAdapter(makeSignUpController()))
 
 app.listen(3001, () => {
     console.log('Server Running')
