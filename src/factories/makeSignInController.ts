1b58fe43adaeb800d99c832ec039dbde84fe78c1
Revoltzin <jat1963@gmail.com>
2026-03-16 21:13:39 -0300

    factory created for SignInController

---
+++ src/factories/makeSignInController.ts
@@ -0,0 +1,8 @@
+import { SignInController } from "../application/controllers/SignInController.js";
+import { makeSignInUseCase } from "./makeSignInUseCase.js";
+
+export function makeSignInController () {
+    const signInUseCase = makeSignInUseCase()
+
+    return new SignInController(signInUseCase)
+}
